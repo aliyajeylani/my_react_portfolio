bf1a3779aa4b636b84dfb39c4873a67f9c2539a7
aliyajeylani <aliya.jeylani24@gmail.com>
2023-05-07 12:59:15 -0500

    fixing issue with bootstrap carousel

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,12 +1,13 @@
 
 import Carousel from 'react-bootstrap/Carousel';
+import React from 'react';
 import '../styles/Portfolio.css';
 
 
 
 const styles = {
-  sectionStyle: {
-
+  text: {
+    textAlign: 'center',
   },
 
 }
@@ -16,82 +17,51 @@ function Portfolio() {
 
 
   return (
-    // <section className="portfolio">
-    //   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    //     <ol class="carousel-indicators">
-    //       <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    //       <li data-target="#carouselExampleIndicators" data-slide-to="1" class=""></li>
-    //       <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    //     </ol>
-    //     <div class="carousel-inner">
-    //       <div class="carousel-item active">
-    //         <a href="https://aliyajeylani.github.io/job_finder/">
-    //           <img src="itakeoff_webpage.png" class="d-block w-100" alt="itakeoff webpage" />
-    //         </a>
-    //       </div>
-    //       <div class="carousel-item">
-    //         <a href="https://aliyajeylani.github.io/Code_quiz_app/">
-    //           <img src="code_quiz.png" class="d-block w-100" alt="code quiz challenge" />
-    //         </a>
-    //       </div>
-    //       <div class="carousel-item">
-    //       <a href="https://drive.google.com/file/d/1c_6ipJ8W3_3FVSShYrbeHjrQTMELGq8k/view">
-    //         <img src="team_profile.png" class="d-block w-100" alt="team profile challenge" />
-    //         </a>
-    //       </div>
-    //     </div>
-    //     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    //       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    //       <span class="sr-only">Previous</span>
-    //     </a>
-    //     <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    //       <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    //       <span class="sr-only">Next</span>
-    //     </a>
-    //   </div>
+    <section className="portfolio">
+      <Carousel>
+        <Carousel.Item>
+          <a href="https://aliyajeylani.github.io/job_finder/">
+            <img
+              className="d-block w-100"
+              src="itakeoff_webpage.png"
+              alt="iTakeoff Webpage"
+            />
+          </a>
+          <Carousel.Caption>
+            <h3 styles={styles.text}>iTakeOff</h3>
+            <p>I created a job explorer website called iTakeoff for people who are new to tech and would like to learn more about some of the most popular jobs in the tech industry.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="code_quiz.png"
+            alt="Code Quiz Challenge"
+          />
 
-    <Carousel>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=First slide&bg=373940"
-        alt="First slide"
-      />
-      <Carousel.Caption>
-        <h3>First slide label</h3>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=Second slide&bg=282c34"
-        alt="Second slide"
-      />
+          <Carousel.Caption>
+            <h3>Code Quiz</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="team_profile.png"
+            alt="Team Profile Challenge"
+          />
 
-      <Carousel.Caption>
-        <h3>Second slide label</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=Third slide&bg=20232a"
-        alt="Third slide"
-      />
+          <Carousel.Caption>
+            <h3>Team Profile</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
 
-      <Carousel.Caption>
-        <h3>Third slide label</h3>
-        <p>
-          Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-        </p>
-      </Carousel.Caption>
-    </Carousel.Item>
-  </Carousel>
+    </section>
 
-
-    // </section>
   );
 }
 
